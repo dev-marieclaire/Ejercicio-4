848653f88e1bda55d9500796ae778294709d03c6
dev-marieclaire <mail.maryjamesb@gmail.com>
2026-07-03 14:39:17 -0600

    Cambios, agregación de PlanSuscipción en CuentaUsuario

--- src/CuentaUsuario.java
+++ src/CuentaUsuario.java
@@ -2,5 +2,6 @@ public abstract class CuentaUsuario {
     private String correoelectronic;
     private int mesesactivo;
 
-    
+    // Añadiendo Plan dentro de la clase abstracta.
+    iPlanSuscripcion PlanSuscripcion;
 }
