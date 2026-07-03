b18a268aaf6cc447a7144af1536a3b5d77eee3f8
Stormy <memeanimatic@gmail.com>
2026-07-03 14:40:21 -0600

    arreglando clases y agregando la abstracta ☺️☺️☺️

--- src/CuentaUsuario.java
+++ src/CuentaUsuario.java
@@ -2,5 +2,9 @@ public abstract class CuentaUsuario {
     private String correoelectronic;
     private int mesesactivo;
 
-    
+
+    public CuentaUsuario(String correoelectronic, int mesesactivo) {
+        this.correoelectronic = correoelectronic;
+        this.mesesactivo = mesesactivo;
+    }
 }
