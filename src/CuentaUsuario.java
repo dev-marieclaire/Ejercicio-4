066fd23458b87baaf938f42ffd31675e4eb493d1
Stormy <memeanimatic@gmail.com>
2026-07-05 11:49:24 -0600

    arreglando clases y agregando la abstracta ☺️☺️☺️

--- src/CuentaUsuario.java
+++ src/CuentaUsuario.java
@@ -1,12 +1,18 @@
-public abstract class CuentaUsuario {
+public abstract class CuentaUsuario implements iPlanSuscripcion {
     private String correoelectronic;
     private int mesesactivo;
 
     // Añadiendo Plan dentro de la clase abstracta.
     iPlanSuscripcion PlanSuscripcion;
 
-    public CuentaUsuario(String correoelectronic, int mesesactivo) {
+    public CuentaUsuario(String correoelectronic, int mesesactivo, iPlanSuscripcion planSuscripcion) {
         this.correoelectronic = correoelectronic;
         this.mesesactivo = mesesactivo;
+        PlanSuscripcion = planSuscripcion;
+    }
+
+    // Con este metodo luego le agregamos para que llame al ooooootro metodo
+    public void ObtenerTotalPagar (){
+
     }
 }
